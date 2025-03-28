949e4b7f3f59caa26058449fd0d100e10ee41486
BI66IE619 <146889677+BI66IE619@users.noreply.github.com>
2025-03-27 17:57:48 -0700

    Update script.js

--- tests/Bttns/script.js
+++ tests/Bttns/script.js
@@ -1,1 +1,8 @@
-
+document.querySelectorAll(".nav-item").forEach(item => {
+    item.addEventListener("mouseover", () => {
+        item.classList.add("floating");
+    });
+    item.addEventListener("mouseleave", () => {
+        item.classList.remove("floating");
+    });
+});
