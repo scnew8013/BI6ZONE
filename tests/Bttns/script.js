27bb457b5c508f009501a0412d5278fad5c9e002
BI66IE619 <146889677+BI66IE619@users.noreply.github.com>
2025-03-27 18:35:40 -0700

    Delete tests/Bttns/script.js

--- tests/Bttns/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".nav-item").forEach(item => {
-    item.addEventListener("mouseover", () => {
-        item.classList.add("floating");
-    });
-    item.addEventListener("mouseleave", () => {
-        item.classList.remove("floating");
-    });
-});
