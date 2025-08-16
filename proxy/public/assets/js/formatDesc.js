659fe31ba70447023d9f24f00574dcff17c23fb3
BI66IE619 <146889677+BI66IE619@users.noreply.github.com>
2025-08-15 23:37:14 -0700

    Delete proxy directory

--- proxy/public/assets/js/formatDesc.js
+++
@@ -1,8 +0,0 @@
-// Enables max character limit on desc for games & apps
-var i;
-var divs = document.getElementsByClassName('description');
-for (i = 0; i < divs.length; i++) {
-  if (divs[i].className == 'description') {
-    divs[i].innerHTML = divs[i].innerHTML.substring(0, 45) + '...';
-  }
-}
