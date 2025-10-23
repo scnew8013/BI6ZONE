d782abd3a345e76408979e16ceb91e2c28a77d18
scnew8013 <scnew8013@gmail.com>
2025-10-23 23:34:52 +0000

    update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -20,22 +20,23 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "astro-build.astro-vscode"
     ];
 
     # Enable previews
     previews = {
       enable = true;
       previews = {
-        # web = {
+         web = {
         #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
         #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        #   command = ["bun" "run" "dev"];
+           manager = "web";
+           env = {
+             # Environment variables to set for your server
+             PORT = "$PORT";
+           };
+         };
       };
     };
 
